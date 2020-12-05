894804d8f99b09f458cac980dc59bf18fd4265c2
yfove <curtis.ch.chat@gmail.com>
2020-12-05 16:28:24 -0700

    setup update list on Lists component

--- src/components/Lists.js
+++ src/components/Lists.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import styled from "styled-components";
 
 // Import from inputs
@@ -6,9 +6,25 @@ import ListInput from "./Inputs/List";
 import CardInput from "./Inputs/Card";
 
 export default () => {
+  // setState
+  const [setLists] = useState([]);
+  useEffect(() => {
+    let json = localStorage.getItem("lists");
+    setLists(json === null ? [] : JSON.parse(json));
+    if (json === null) localStorage.setItem("lists", JSON.stringify([]));
+  }, []);
+
+  //   CRUD Funtions
+  const updateList = (data) => {
+    setLists(data);
+    saveList(data);
+  };
+
   return (
-    <div>
-      <h1>hello</h1>
-    </div>
+    <List>
+      <ListInput updateList={updateList} />
+    </List>
   );
 };
+
+//styling
